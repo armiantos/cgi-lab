baafdbb71d1f95efe7fe78e76bc59b23a52bff0c
Armianto Sumitro <armiantos@gmail.com>
2022-01-24 18:31:37 -0700

    Pretty print questions 1-3

--- hello.py
+++ hello.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+import json
 import os
 
 print('Content-Type: text/html')
 print()
-print(f"<p>{os.environ['HTTP_USER_AGENT']}</p>")
+
+payload = {
+    # Question 1
+    'environment_variables': dict(os.environ),
+    # Question 2
+    'query_parameter': os.environ['HTTP_USER_AGENT'],
+    # Question 3
+    'browser_info': os.environ['HTTP_USER_AGENT']
+}
+
+for section_title, contents in payload.items():
+    print(f'''
+    <h1>{section_title}</h1>
+    <pre>{contents}</pre>
+    ''')
